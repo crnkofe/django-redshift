872acda94b28c1d0cd94551cc1ce0833d33bab78
Simon Mihevc <simon.mihevc@zemanta.com>
2015-08-21 14:41:05 +0200

    Requirements updated and added to setup

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(
     name='django-redshift',
     version='0.2',
-    description='Amazon Redshift Dialect for django',
+    description='Amazon Redshift Django Backend',
     long_description=open("README.rst").read(),
     author='Matt George',
     author_email='mgeorge@gmail.com',
     license="MIT",
     url='https://github.com/binarydud/django-redshift',
     packages=['django-redshift'],
-    install_requires=['psycopg2==2.5'],
+    install_requires=['psycopg2==2.6.1'],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
